9ad8b6d44ee40fcb426b8992154ebbcf0dbb9337
Pravartak <pravartakambhore@gmail.com>
2025-12-13 12:11:21 +0530

    Auto redirect logged in users to home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+"use client";
+
 import { Navbar } from "@/components/navbar"
 import { Hero } from "@/components/hero"
 import { Testimonials } from "@/components/testimonials"
 import { Footer } from "@/components/footer"
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if(typeof window !== 'undefined') {
+      const username = localStorage.getItem("username");
+
+      if(username) {
+        router.push("/home");
+      }
+      return;
+    }
+  }, []);
   return (
     <>
       <Navbar />
