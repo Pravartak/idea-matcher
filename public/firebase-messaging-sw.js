4a79c04d080a8673df682e8c10ebb300abe9b8a6
Pravartak <pravartakambhore@gmail.com>
2026-02-19 22:34:40 +0530

    Notifications check

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+import { messaging } from 'firebase-admin';
+import compat from 'firebase/compat/app';
+import 'firebase/compat/messaging';
+
+firebase.initializeApp({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    self.registration.showNotification(payload.notification.title, {
+        body: payload.notification.body,
+        icon: payload.notification.icon,
+    });
+});
