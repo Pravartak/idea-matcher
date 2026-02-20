dbe8d9969a5050b5b699d4f2d1d8f613395e3b05
Pravartak <pravartakambhore@gmail.com>
2026-02-20 23:56:20 +0530

    update to firebase-messaging-sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,20 +1,28 @@
-import { messaging } from 'firebase-admin';
-import compat from 'firebase/compat/app';
-import 'firebase/compat/messaging';
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
 firebase.initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBBIiK9E634aaF5lLC21751YbxyOZUXCDY",
+  authDomain: "idea-matcher.firebaseapp.com",
+  projectId: "idea-matcher",
+  storageBucket: "idea-matcher.firebasestorage.com",
+  messagingSenderId: "888281953245",
+  appId: "1:888281953245:web:279bbf8ede7e7ca55c3c34",
 });
 
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-    self.registration.showNotification(payload.notification.title, {
-        body: payload.notification.body,
-        icon: payload.notification.icon,
-    });
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png' // Replace with your app icon path if you have one
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
